009a80675356e0392f912345fb4abb48ea7f36ce
Pablo <puribe95@hotmail.com>
2020-04-24 15:17:17 -0500

    broker connect

---
+++ include/constants.h
@@ -0,0 +1,10 @@
+#define SSID "GUribe"
+#define SSID_PASSWORD "816048100"
+
+#define MQTT_ID "PU_esp32"
+#define MQTT_USER "Arduino"
+#define MQTT_PASSWORD "PASSWORD"
+
+#define BROKER "192.168.1.65"
+#define REPORT_FREQ 20000
+#define MAX_ALARM_FREQ 5000
